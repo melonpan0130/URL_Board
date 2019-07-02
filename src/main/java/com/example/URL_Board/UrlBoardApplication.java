aaaa0662e9d6404a3fd4a988bbd055a4eb70180f
Eunju/kiori <eun01300714@gmail.com>
2019-07-03 08:19:10 +0900

    first commit

---
+++ src/main/java/com/example/URL_Board/UrlBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.URL_Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlBoardApplication.class, args);
+	}
+
+}
